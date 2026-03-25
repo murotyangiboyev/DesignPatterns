003be776ff7eeb7583276baaecf11b0a3f197216
Murot Yangiboyev <murotyangiboyev77@gmail.com>
2026-03-25 12:03:20 +0500

    Singleton Pattern

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        SingletonPattern singletonPattern =  SingletonPattern.getInstance();
+        singletonPattern.query("Singleton pattern 1");
+
+        SingletonPattern singletonPattern2 =  SingletonPattern.getInstance();
+        singletonPattern2.query("Singleton pattern 2");
+
+        System.out.println(singletonPattern == singletonPattern2);
+
+    }
+}
---
+++ src/SingletonPattern.java
@@ -0,0 +1,21 @@
+
+
+public class SingletonPattern {
+
+    private static SingletonPattern instance;
+
+    private SingletonPattern() {
+        System.out.println("Creating Singleton Pattern");
+    }
+
+    public static synchronized SingletonPattern getInstance() {
+        if (instance == null) {
+            instance = new SingletonPattern();
+        }
+        return instance;
+    }
+
+    public void query(String s){
+        System.out.println(s);
+    }
+}
